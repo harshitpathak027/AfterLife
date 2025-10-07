32b35d07db5801b7f0222ffd7dc40c9cbdc53928
harshitpathak027 <harshitpathak027@gmail.com>
2025-10-07 18:41:52 +0530

    file committed

---
+++ src/main/java/com/example/AfterLife/AfterLifeApplication.java
@@ -0,0 +1,15 @@
+package com.example.AfterLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterLifeApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(AfterLifeApplication.class, args);
+
+	}
+
+}
